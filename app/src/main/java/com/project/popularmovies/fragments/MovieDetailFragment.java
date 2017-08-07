045c963e2076a0b8a711c0ed01bc0a3ef9a2b56c
Chetan <chetan.kh23@gmail.com>
2017-08-06 21:13:21 -0700

    Replaced fragment

---
+++ app/src/main/java/com/project/popularmovies/fragments/MovieDetailFragment.java
@@ -0,0 +1,10 @@
+package com.project.popularmovies.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by chetan on 8/5/17.
+ */
+
+public class MovieDetailFragment extends Fragment {
+}
